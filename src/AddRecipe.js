634e6277433895eb8978aa169a781f71d612bcc4
Gopi Kishore <kishorepenjendra@gmail.com>
2021-11-02 15:47:17 +0530

    checkobx  updated

--- src/AddRecipe.js
+++ src/AddRecipe.js
@@ -38,27 +38,27 @@ function AddRecipe() {
             <div className="container">
                 <form onSubmit={handleSubmit}>
                     <div className="row">
-                        <div className="col col-md-5">
+                        <div className="col col-sm-5">
                             <label>Recipe Name</label>
                             <input type="text" value={recipe} required onChange={(e)=>{setRecipe(e.target.value)}} className="form-control"/>
                         </div>
-                        <div className="col col-md-5">
+                        <div className="col col-sm-5">
                             <label>Ingredients</label>
                             <input type="text" value={ingredients} required onChange={(e)=>{setIngredients(e.target.value)}} className="form-control"/>
                         </div>
-                        <div className="col col-md-2">
+                        <div className="col col-2">
                             <label>is Veg</label>
-                            <input type="checkbox" className="form-check-input me-1" checked={isVeg} onChange={e=>{setVeg(!isVeg)}} className="form-control"/>
+                            <input type="checkbox"  className="form-control form-check-input me-1" checked={isVeg} onChange={e=>{setVeg(!isVeg)}} />
                         </div>
-                        <div className="col col-md-12">
+                        <div className="col col-sm-12">
                             <label>Procedure</label>
                             <input type="text" value={procedure} required onChange={(e)=>{setProcedure(e.target.value)}} className="form-control"/>
                         </div>
-                        <div className="col col-md-12">
+                        <div className="col col-sm-12">
                             <label>Add an Image URL</label>
                             <input type="text" value={url} required onChange={(e)=>{setUrl(e.target.value)}} className="form-control"/>
                         </div>
-                        <div className="col col-md-12">
+                        <div className="col col-sm-12">
                             <input type="submit" value="Submit" className="btn btn-primary mt-3" disabled={isLoading}/>
                         </div>
                     </div>
