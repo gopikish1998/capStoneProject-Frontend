f35a2fe70d66def58f45b0434e637b76574f5574
Gopi Kishore <kishorepenjendra@gmail.com>
2022-01-19 03:46:08 +0530

    updated

--- src/Welcome.js
+++ src/Welcome.js
@@ -54,7 +54,7 @@ function Welcome() {
         <div class="container px-4 px-lg-5">
             <div class="row gx-4 gx-lg-5 align-items-center my-5">
                 <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="https://media.istockphoto.com/photos/varied-food-carbohydrates-protein-vegetables-fruits-dairy-legumes-on-picture-id1218254547?b=1&k=20&m=1218254547&s=170667a&w=0&h=mOEC7x7qU5NC78mCULs-jAPeLkxy8opOvIbGSnwmAyw=" alt="..." /></div>
-                <div class="col-lg-5">
+                <div class="col-lg-5" style={{color:'darkblue'}}>
                     <h1 class="font-weight-light">Make or Browse Recipes</h1>
                     <p>This website helps you to manage your grocery checklist and browse for Recipes available. You can also contribute to us by adding your Recipes!  </p>
                     {/* <a class="btn btn-primary" href="#!">Call to Action!</a> */}
@@ -73,7 +73,7 @@ function Welcome() {
             </div> */}
         </div>
         <footer class="py-5 bg-dark">
-            <div class="container px-4 px-lg-5"><p class="m-0 text-center text-white">Copyright &copy; Recipe Management by Gopi 2021</p></div>
+            <div class="container px-4 px-lg-5" ><p class="m-0 text-center text-white">Copyright &copy; Recipe Management by Gopi 2021</p></div>
         </footer>
 
         </div>
